18e3ea87d816c023a93f12e1ab240d578dab7959
Johan BONASSI <johan.bonassi@icloud.com>
2025-01-24 00:19:58 +0100

    Disable tokyonight colorscheme

--- lua/jobenni/plugins/colorscheme-tokyonight.lua
+++ lua/jobenni/plugins/colorscheme-tokyonight.lua
@@ -5,33 +5,34 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    enabled = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-        -- Load the colorscheme here.
-        -- Like many other themes, this one has different styles, and you could load
-        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'tokyonight-night'
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
 
-        -- You can configure highlights by doing something like:
-        --vim.cmd.hi 'Comment gui=none'
+      -- You can configure highlights by doing something like:
+      --vim.cmd.hi 'Comment gui=none'
     end,
     config = function()
       local transparent = false -- set to true if you would like to enable transparency
 
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
+      local bg_highlight = '#143652'
+      local bg_search = '#0A64AC'
+      local bg_visual = '#275378'
+      local fg = '#CBE0F0'
+      local fg_dark = '#B4D0E9'
+      local fg_gutter = '#627E97'
+      local border = '#547998'
 
-      require("tokyonight").setup({
-        style = "night",
+      require('tokyonight').setup {
+        style = 'night',
         transparent = transparent,
         styles = {
-          sidebars = transparent and "transparent" or "dark",
-          floats = transparent and "transparent" or "dark",
+          sidebars = transparent and 'transparent' or 'dark',
+          floats = transparent and 'transparent' or 'dark',
         },
         on_colors = function(colors)
           colors.bg_highlight = bg_highlight
@@ -44,8 +45,8 @@ return {
           colors.fg_gutter = fg_gutter
           colors.fg_sidebar = fg_dark
         end,
-      })
-    end
+      }
+    end,
   },
 }
 

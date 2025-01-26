d17f4b01aac4302b000a184d92cb26f42e29af95
Johan BONASSI <johan.bonassi@icloud.com>
2025-01-26 09:37:45 +0100

    LazyVim starter template

--- lua/jobenni/core/init.lua
+++
@@ -1,2 +0,0 @@
-require 'jobenni.core.options'
-require 'jobenni.core.keymaps'
--- lua/jobenni/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'christoomey/vim-tmux-navigator', -- Seamless navigation between tmux panes and vim splits
-  { 'akinsho/toggleterm.nvim', version = '*', config = true },
-}

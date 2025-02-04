e15f0b1146d66619f123fa986d52f325ae254c26
Johan BONASSI <johan.bonassi@icloud.com>
2025-02-04 02:00:30 +0100

    Use catppuccin colorscheme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+    },
+  },
+}

d70e55cd1eda0e5c1d5718217ca647bd51f13f37
Johan BONASSI <johan.bonassi@icloud.com>
2025-12-07 17:44:09 +0100

    Enable clippy on the fly for Rust

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -5,9 +5,18 @@ return {
       server = {
         default_settings = {
           ["rust-analyzer"] = {
+            checkOnSave = {
+              command = "clippy",
+            },
             procMacro = {
               ignored = {
                 ["async-trait"] = vim.NIL,
+              },
+            },
+            diagnostics = {
+              enable = true,
+              experimental = {
+                enable = true,
               },
             },
           },
